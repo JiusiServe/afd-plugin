b8ce0201b5758c559e2357770101104bfcc80ec4
bjf-frz <frz123db@gmail.com>
2026-08-25 15:19:12 +0800

    Remove local experiment docs and scripts from PR
    Signed-off-by: bjf-frz <frz123db@gmail.com>

--- tools/itask/run_itask.sh
+++
@@ -1,8 +0,0 @@
-name=${1:?usage: run_itask.sh <task-name> <model-image>}
-model=${2:?usage: run_itask.sh <task-name> <model-image>}
-user=wb02363348
-#image=hcr.met:a-wulan01.hw-wulan.local/antsys/vllm:release_0.18.0_0415_202603221557_aarch64
-image=hcr.meta-wulan01.hw-wulan.local/antsys/vllm:nightly-main-a3-openeuler-20260801230444_aarch64
-itask create --name ${name} --user ${user} --model ${model} --image ${image} --hostnet --16card --skip-sync --type a3 --workdir /a3_inference/itask/workdir/wb02363348/bjf_afd/code
-# itask create --name ${name} --user ${user} --image ${image} --hostnet --16card --skip-sync --type a3 --workdir /a3_inference/itask/workdir/hk02335263/jcz_afd_100/code
-# itask create --name ${name} --user ${user} --image ${image} --hostnet --16card --skip-sync --type a3
